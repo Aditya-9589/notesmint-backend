fb6497c6a043f57d155e43ad79e5e064b2341205
Aditya Hariharno <work.adityah@gmail.com>
2026-03-24 17:59:04 +0530

    bundle model edited

--- models/Bundle.js
+++ models/Bundle.js
@@ -23,6 +23,7 @@ const bundleSchema = new mongoose.Schema(
             {
                 title: { type: String, required: true },
                 pdfUrl: { type: String, required: true },
+                pdfKey: { type:String, required: true },
             },
         ],
     },
